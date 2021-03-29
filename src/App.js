9d79f171623e9f9aee4fff4d8317f3d8cfed4e85
Juan Guzman <juan.guzman.sd@gmail>
2021-03-29 12:41:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import VendingMachine from "./VendingMachine";
+import Sardines from "./Sardines";
+import Chips from "./Chips";
+import Soda from "./Soda";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <VendingMachine />
+        </Route>
+        <Route path="/soda" exact>
+          <Soda />
+        </Route>
+        <Route path="/chips" exact>
+          <Chips />
+        </Route>
+        <Route path="/sardines" exact>
+          <Sardines />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
